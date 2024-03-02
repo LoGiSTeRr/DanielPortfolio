a24406162748620d3dd9c7b78b55bc00901a2d69
Faxraddin <reydivonay@gmail.com>
2024-03-02 22:06:17 +0400

    colors

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} font-mont`}>{children}</body>
+      <body className={`${inter.className} font-mont bg-light w-full min-h-screen`}>{children}</body>
     </html>
   );
 }
