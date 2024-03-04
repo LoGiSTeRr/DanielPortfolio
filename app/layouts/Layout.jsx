25d7c229be1baca76faecce985d58a5380d492ee
Faxraddin <reydivonay@gmail.com>
2024-03-04 22:53:52 +0400

    layout

---
+++ app/layouts/Layout.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Layout = ({children, className=""}) => {
+  return (
+    <div className={`w-full p-32 h-full inline-block z-0 bg-light ${className}`}>
+        {children}
+    </div>
+  )
+}
+export default Layout
--- app/page.js
+++ app/page.js
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Navbar from "./layouts/Navbar";
+import Layout from "./layouts/Layout";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+
+      <div className="flex items-center text-dark w-full min-h-screen">
+        <Layout>
+          <h1>hjnkml</h1>
+        </Layout>
+      </div>
     </main>
   );
 }
