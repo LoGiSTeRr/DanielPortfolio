5b96e9ca59663ed956d9ae30a34e73892acc43d3
Faxraddin <reydivonay@gmail.com>
2024-03-04 23:22:34 +0400

    home page p2

--- app/page.js
+++ app/page.js
@@ -3,10 +3,11 @@ import Navbar from "./layouts/Navbar";
 import Layout from "./layouts/Layout";
 
 import ProfilePicture from '../public/images/profile/developer-pic-1.png'
+import AnimatedText from "./components/AnimatedText";
 
 export default function Home() {
   return (
-    <main className="bg-light text-dark" >
+    <main className="bg-light text-dark">
       <Navbar />
 
       <div className="flex items-center text-dark w-full min-h-screen">
@@ -18,6 +19,18 @@ export default function Home() {
                 alt="Daniel"
                 className="h-auto w-full"
               />
+            </div>
+            <div className="w-1/2 flex flex-col items-center self-center">
+              <AnimatedText
+                className="text-6xl"
+                text="Turning Vision Into Reality With Code And Design. "
+              />
+              <p>
+                As a skilled full-stack developer, I am dedicated to turning
+                ideas into innovative web applications. Explore my latest
+                projects and articles, showcasing my expertise in React.js and
+                web development.
+              </p>
             </div>
           </div>
         </Layout>
