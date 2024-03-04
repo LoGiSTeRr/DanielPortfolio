6b7e13446f873c35c5b34eff88e770f75804ce86
Faxraddin <reydivonay@gmail.com>
2024-03-04 23:00:44 +0400

    home page

--- app/page.js
+++ app/page.js
@@ -2,14 +2,24 @@ import Image from "next/image";
 import Navbar from "./layouts/Navbar";
 import Layout from "./layouts/Layout";
 
+import ProfilePicture from '../public/images/profile/developer-pic-1.png'
+
 export default function Home() {
   return (
-    <main>
+    <main className="bg-light text-dark" >
       <Navbar />
 
       <div className="flex items-center text-dark w-full min-h-screen">
-        <Layout>
-          <h1>hjnkml</h1>
+        <Layout className="pt-0">
+          <div className="flex items-center justify-between w-full">
+            <div className="w-1/2">
+              <Image
+                src={ProfilePicture}
+                alt="Daniel"
+                className="h-auto w-full"
+              />
+            </div>
+          </div>
         </Layout>
       </div>
     </main>
