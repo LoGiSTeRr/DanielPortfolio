3a18fe6fcbbac6f212710a7f0f6a266f71c67c8d
Faxraddin <reydivonay@gmail.com>
2024-03-04 21:53:03 +0400

    spave

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,30 +1,35 @@
+'use client';
+
 import Link from "next/link"
 import React from "react"
 import Logo from "./Logo"
+import { useRouter } from "next/navigation";
 
 const Navbar = () => {
-  return (
-    <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
-        <nav>
-            <Link className="relative group" href="/">Home <span className='h-[1px] group-hover:w-full absolute left-0 -bottom-0.5 inline-block w-0 ease duration-300 transition-[width] bg-dark'>&nbsp;</span></Link>
-            <Link href="/about">About</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/articles">Articles</Link>
-        </nav>
-        
-        <nav>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-            <Link href="/" target={"_blank"}>T</Link>
-        </nav>
+    const router = useRouter();
 
-        <div className='absolute left-[50%] top-2 gtranslate-x-[-50%]'>
-            <Logo/>
-        </div>
-    </header>
-  )
+    return (
+        <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
+            <nav>
+                <Link className="relative group mr-4" href="/">Home <span className={`${router.asPath === '/' ? 'w-full':'w-0'} h-[1px] group-hover:w-full absolute left-0 -bottom-0.5 inline-block ease duration-300 transition-[width] bg-dark`}>&nbsp;</span></Link>
+                <Link className="relative group mx-4" href="/about">About</Link>
+                <Link className="relative group mx-4" href="/projects">Projects</Link>
+                <Link className="relative group ml-4" href="/articles">Articles</Link>
+            </nav>
+            
+            <nav>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+                <Link href="/" target={"_blank"}>T</Link>
+            </nav>
+
+            <div className='absolute left-[50%] top-2 gtranslate-x-[-50%]'>
+                <Logo/>
+            </div>
+        </header>
+    )
 }
 export default Navbar
