2e85663382bbd41946268aa721e74915e34177d6
Faxraddin <reydivonay@gmail.com>
2024-03-06 20:44:30 +0400

    hire me done

--- app/sections/main/HireMe.jsx
+++ app/sections/main/HireMe.jsx
@@ -7,7 +7,7 @@ const HireMe = () => {
         <div className='fixed left-4 bottom-4 flex items-center justifyfcenter overflow-hidden'>
           <div className='w-48 h-auto flex items-center justify-center relative'>
             <CircularText className={'fill-black animate-spin-slow'}/>
-            <Link href="mailto:abcd@gmail.com" className='flex items-center justify-center absolute 1eft-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark h-20 w-20 rounded-full'>
+            <Link href="mailto:abcd@gmail.com" className='flex items-center justify-center absolute 1eft-1/2 top-1/2 -translate-x-0 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark h-20 w-20 rounded-full'>
               Hire Me
             </Link>
           </div>
