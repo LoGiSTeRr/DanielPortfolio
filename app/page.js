57b7516c1f7421172c600f013574d845eaa629a0
Faxraddin <reydivonay@gmail.com>
2024-03-06 20:50:44 +0400

    bulb

--- app/page.js
+++ app/page.js
@@ -4,13 +4,20 @@ import Footer from "./layouts/Footer";
 import MainPage from "./sections/main/MainPage";
 import HireMe from "./sections/main/HireMe";
 
+import Image from "next/image";
+import lightBulb from '../public/images/svgs/miscellaneous_icons_1.svg'
+
 export default function Home() {
   return (
     <main className="bg-light text-dark">
       <Navbar />
       <div className="flex items-center text-dark w-full min-h-screen">
         <MainPage />
-        <HireMe/>
+
+        <HireMe />
+        <div className=" absolute right-8 bottom-8 inline-block w-20">
+          <Image src={lightBulb} alt="Title" className="w-full h-auto" />
+        </div>
       </div>
       <Footer />
     </main>
