c99a984876e3201256d5245eeb63faf87ffeef5e
Faxraddin <reydivonay@gmail.com>
2024-03-07 17:54:56 +0400

    tc

--- app/about/Skills.jsx
+++ app/about/Skills.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const Skills = () => {
   return (
-    <div>
-        
-    </div>
+    <>
+        <h2 className='font-bold text-8x1 mt-64 w-full text-center'>Skills</h2>
+        <div>
+            
+        </div>
+    </>
   )
 }
 export default Skills
