1ad829ab767a721f329b6bfef4590419d7566249
Faxraddin <reydivonay@gmail.com>
2024-03-09 21:56:03 +0400

    done

--- app/layouts/Layout.jsx
+++ app/layouts/Layout.jsx
@@ -1,13 +1,13 @@
-'use client'
-import React from "react"
+'use client';
+import React, { Component } from "react"
 import { AnimatePresence } from "framer-motion"
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 
 const Layout = ({children, className=""}) => {
-  const router = useRouter
+  const router = useRouter()
   return (
     <div className={`w-full p-32 h-full inline-block z-0 dark:bg-dark xl:p-24 1g:p-16 md:p-12 sm:p-8 bg-light ${className}`}>
-      <AnimatePresence key={router.asPath}>
+      <AnimatePresence mode="wait" key={router.asPath}>
           {children}
       </AnimatePresence>
     </div>
