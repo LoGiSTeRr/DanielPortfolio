eea5d5b194146df623d0d17cdba40ceecc7aa3e0
Faxraddin <reydivonay@gmail.com>
2024-03-09 19:24:48 +0400

    done

--- components/LiIcon.jsx
+++ components/LiIcon.jsx
@@ -8,7 +8,7 @@ const LiIcon = ({reference}) => {
     })
     return (
         <figure className=' absolute left-0 stroke-dark dark:stroke-light'>
-            <svg className="-rotate-90" width={'75'} height={'75'} viewBox="0 0 100 100">
+            <svg className="-rotate-90 md:w-[60px] md:h-[60px] xs:w-[43px] xs:h-[47px]" width={'75'} height={'75'} viewBox="0 0 100 100">
                 <circle cx='75' r={20} cy='50' className='dark:stroke-primaryDark stroke-primary stroke-1 fill-none'/>
                 <motion.circle style={{pathLength:scrollYProgress}} cx='75' r={20} cy='50' className='dark:fill-dark stroke-[5px] fill-light'/>
                 <circle cx='75' r={10} cy='50' className=' animate-pulse stroke-1 fill-primary dark:fill-primaryDark'/>
