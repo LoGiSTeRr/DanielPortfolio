e33ba5bb7011a6f5b3ef52a7ae91e4ae94ecdd91
Faxraddin <reydivonay@gmail.com>
2024-03-09 19:08:43 +0400

    sjills res

--- app/about/Skills.jsx
+++ app/about/Skills.jsx
@@ -3,7 +3,11 @@ import {motion} from 'framer-motion'
 
 const Skill = ({name,x,y}) => {
     return(
-        <motion.div initial={{x:0,y:0}} whileInView={{x:x,y:y,transition:{duration: 1.5}}} whileHover={{scale:1.05}} className='p-8 cursor-pointer shadow-dark flex items-center justify-center rounded-full dark:text-dark dark:bg-light font-semibold bg-dark text-light absolute px-6 py-3'>
+        <motion.div initial={{x:0,y:0}} whileInView={{x:x,y:y,transition:{duration: 1.5}}} whileHover={{scale:1.05}} className='p-8 cursor-pointer shadow-dark flex items-center justify-center rounded-full dark:text-dark dark:bg-light font-semibold bg-dark text-light absolute px-6 py-3
+        lg:py-2 lg:px-4 md:text-sm md:py-1.5 md:px-3 xs:bg-transparent xs:dark:bg-transparent
+        xs:text-dark xs:dark:text-light xs:font-bold
+        
+        '>
             {name}
         </motion.div>
     )
@@ -12,9 +16,15 @@ const Skill = ({name,x,y}) => {
 const Skills = () => {
   return (
     <>
-        <h2 className='font-bold text-8xl mt-64 w-full text-center'>Skills</h2>
-        <div className='w-full h-screen relative flex items-center justify-center rounded-full dark:bg-circularDark bg-circularLight'>
-            <motion.div whileHover={{scale:1.05}} className='p-8 cursor-pointer shadow-dark flex items-center justify-center rounded-full font-semibold dark:bg-light dark:text-dark bg-dark text-light'>
+        <h2 className='font-bold text-8xl mt-64 w-full text-center md:text-6xl md:mt-32'>Skills</h2>
+        <div className='w-full h-screen relative flex items-center justify-center rounded-full dark:bg-circularDark bg-circularLight
+        lg:h-[80vh] sm:h-[60vh] xs:h-[44vh] lg:bg-circularLightLg lg:dark:bg-circularDarkLg
+        md:bg-circularLightMd md:dark:bg-circularDarkMd
+        sm:bg-circularLightSm sm:dark:bg-circularDarkSm
+        '>
+            <motion.div whileHover={{scale:1.05}} className='p-8 cursor-pointer shadow-dark flex items-center justify-center rounded-full font-semibold dark:bg-light dark:text-dark bg-dark text-light
+            lg:p-6 md:p-4 xs:text-xs xs:p-2
+            '>
                 Web
             </motion.div>
             <Skill name={'CSS'} x="-5vw" y="-10vw"/>
