2e51879f1862088a6d26cd2bc0d7acf9f6e6c188
Faxraddin <reydivonay@gmail.com>
2024-03-09 20:53:09 +0400

    its done lol

--- app/articles/page.jsx
+++ app/articles/page.jsx
@@ -30,16 +30,18 @@ const MovingImg = ({title,img,link}) => {
     return(
         <Link onMouseMove={handleMouse} onMouseLeave={handleMouseLeave} href={link} target="_blank">
             <h2 className="capitalize text-xl underline-offset-2 font-semibold hover:underline">{title}</h2>
-            <FramerImage initial={{opacity:0}} whileInView={{opacity:1,transition:{duration:0.2}}} style={{x:x, y:y}} ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute rounded-lg"/>
+            <FramerImage initial={{opacity:0}} whileInView={{opacity:1,transition:{duration:0.2}}} style={{x:x, y:y}} ref={imgRef} src={img} alt={title} className="z-10 w-96 h-auto hidden absolute md:!hidden rounded-lg"/>
         </Link>
     )
 }
 
 const Article = ({img,date,title,link}) => {
     return(
-        <motion.li initial={{y:200}} whileInView={{y:0,transition:{duration:0.5}}} viewport={{once:true}} className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between dark:bg-dark dark:text-light dark:border-light bg-light text-dark first:mt-0 border border-dark border-solid border-r-4 border-b-4">
+        <motion.li initial={{y:200}} whileInView={{y:0,transition:{duration:0.5}}} viewport={{once:true}} className="relative w-full p-4 py-6 my-4 rounded-xl flex items-center justify-between dark:bg-dark dark:text-light dark:border-light bg-light text-dark first:mt-0 border border-dark border-solid border-r-4 border-b-4
+        sm:flex-col
+        ">
             <MovingImg title={title} link={link} img={img}/>
-            <span className="text-primary dark:text-primaryDark font-semibold pl-4">{date}</span>
+            <span className="text-primary dark:text-primaryDark font-semibold pl-4 sm:self-start sm:pl-0 xs:text-sm">{date}</span>
         </motion.li>
     )
 }
@@ -59,7 +61,7 @@ const FeaturedArticle = ({img,title,summary,link,time})=> {
                 transition={{duration:0.2}} whileHover={{scale:1.05}} src={img} alt={title} className="w-full h-auto" />
             </Link>
             <Link href={link} target="_blank">
-                <h2 className='capitalize dark:text-light text-2xl mt-2 font-bold my-2 hover:underline underline-offset-2'>
+                <h2 className='capitalize dark:text-light text-2xl mt-2 font-bold my-2 hover:underline underline-offset-2 xs:text-lg'>
                     {title}
                 </h2>
             </Link>
@@ -75,8 +77,8 @@ const page = () => {
   return (
     <main className='w-full text-dark mb-16 flex flex-col items-center justify-center overflow-hidden'>
         <Layout className="pt-16">
-            <AnimatedText className="text-[104px] mb-16" text={'Words Can Change The World! '}/>
-            <ul className='grid grid-cols-2 gap-16'>
+            <AnimatedText className="text-[104px] mb-16 lg:left-0 lg:!text-7xl sm:mb-8 sm:!text-6xl xs:!text-4xl" text={'Words Can Change The World! '}/>
+            <ul className='grid grid-cols-2 gap-16 lg:gap-8 md:grid-cols-1 md:gap-y-16'>
                 <FeaturedArticle
                     title='Build A Custom Pagination Component In Reactjs From Scratch'
                     summary="Learn how to build a custom pagination component in ReactJS from scratch. 
